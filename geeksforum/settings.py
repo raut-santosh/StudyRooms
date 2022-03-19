3f2e5e3859f03f3241620c312aa4484311b23030
santosh raut <rautsantoshtukaram@gmail.com>
2022-03-19 16:53:37 +0530

    created files for heroku and changed settings

--- geeksforum/settings.py
+++ geeksforum/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-je&@_26dr8jfkpx_$7$933zuow@42rafu=7!xo53qnn+useoj^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raut-studyroom.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT = 
+# STATIC_ROOT =
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
